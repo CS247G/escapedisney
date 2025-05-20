23bb37019e8bc8fe3e91537b687b98909053464f
fj0517 <fadumoj@stanford.edu>
2025-05-19 19:54:52 -0700

    Fadumo Upload (all files)

---
+++ escapedisney/fantasy.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var password_input = $PasswordImput
+
+const CORRECT_PASSWORD = "nine"
+const NEXT_SCENE_PATH = "res://adventure.tscn"
+
+func _on_button_pressed() -> void:
+	var entered_password = password_input.text
+
+	if entered_password == CORRECT_PASSWORD:
+		get_tree().change_scene_to_file(NEXT_SCENE_PATH)
